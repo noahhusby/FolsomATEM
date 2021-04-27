55712f24068893319ad8f4661bd7719fec576e12
Noah Husby <noahhusby@gmail.com>
2021-04-27 18:32:33 -0400

    Updated to mega

--- src/main.cpp
+++ src/main.cpp
@@ -13,22 +13,34 @@
  copies or substantial portions of the Software.
  */
 #include <Ethernet.h>
-#include <EthernetUdp.h>
-#include <SPI.h>
 
 #include <OSCBundle.h>
 
-#define ledLatch 8
-#define ledData 11
-#define ledClock 12
-
 #define transitionBar A0
+
+#define ledLatchA 22
+#define ledDataA 24
+#define ledClockA 26
+
+#define ledLatchB 23
+#define ledDataB 25
+#define ledClockB 27
+
+// Latch, Data, Clock
+const int driverA [3] = {ledLatchA, ledDataA, ledClockA};
+const int driverB [3] = {ledLatchB, ledDataB, ledClockB};
 
 // LED Rows
 boolean previewRow [16] = {false};
 boolean programRow [16] = {false};
 boolean auxRow [16] = {false};
 boolean macrowRow [16] = {false};
+
+boolean groupRow [8] = {false};
+boolean keyRow [8] = {false};
+boolean pipRow [8] = {false};
+boolean functionRow [8] = {false};
+
 
 // OSC UDP
 EthernetUDP udp;
@@ -43,17 +55,23 @@ boolean oscEnabled = false;
 double lastTbarValue = 0;
 
 void initOsc();
-void shift(uint8_t b);
+void updateLeds();
+void shift(uint8_t b, const int driver[]);
 void route(OSCBundle &bundle);
+void setPreviewLeds(int value);
+void setProgramLeds(int value);
 void sendMessage(OSCMessage &msg);
 void routeProgram(OSCMessage &msg, int addrOffset);
 uint8_t getFromBits(const boolean arrayA[], const boolean arrayB[], int start);
 
 void setup() {
     //Serial.begin(9600);
-    pinMode(ledLatch, OUTPUT);
-    pinMode(ledClock, OUTPUT);
-    pinMode(ledData, OUTPUT);
+    pinMode(driverA[0], OUTPUT);
+    pinMode(driverA[1], OUTPUT);
+    pinMode(driverA[2], OUTPUT);
+    pinMode(driverB[0], OUTPUT);
+    pinMode(driverB[1], OUTPUT);
+    pinMode(driverB[2], OUTPUT);
 
     pinMode(transitionBar, INPUT);
 
@@ -61,11 +79,50 @@ void setup() {
 
     initOsc();
 
-    // Prepare LEDS
-    digitalWrite(ledData, LOW);
-    digitalWrite(ledClock, LOW);
-    digitalWrite(ledLatch, HIGH);
-}
+    // Prepare LEDs
+    digitalWrite(driverA[0], LOW);
+    digitalWrite(driverA[1], LOW);
+    digitalWrite(driverA[2], LOW);
+    digitalWrite(driverB[0], LOW);
+    digitalWrite(driverB[1], LOW);
+    digitalWrite(driverB[2], LOW);
+
+    for(int i = 0; i < 16; i++) {
+        macrowRow[i] = true;
+        auxRow[i] = true;
+        programRow[i] = true;
+        previewRow[i] = true;
+        updateLeds();
+        delay(50);
+    }
+
+    for(int i = 0; i < 8; i++) {
+        functionRow[i] = true;
+        pipRow[i] = true;
+        keyRow[i] = true;
+        groupRow[i] = true;
+        updateLeds();
+        delay(50);
+    }
+
+    for(int i = 7; i > -1; i--) {
+        functionRow[i] = false;
+        pipRow[i] = false;
+        keyRow[i] = false;
+        groupRow[i] = false;
+        updateLeds();
+        delay(50);
+    }
+
+    for(int i = 15; i > -1; i--) {
+        macrowRow[i] = false;
+        auxRow[i] = false;
+        programRow[i] = false;
+        previewRow[i] = false;
+        updateLeds();
+        delay(50);
+    }
+};
 
 void loop() {
     if(oscEnabled) {
@@ -88,14 +145,13 @@ void loop() {
             msg.empty();
         }
     }
-    delay(20);
 }
 
 /**
  * OSC Methods
  */
 void initOsc() {
-    Ethernet.begin(mac, controllerIp);
+    EthernetClass::begin(mac, controllerIp);
     udp.begin(inPort);
     oscEnabled = true;
 }
@@ -112,13 +168,21 @@ void sendMessage(OSCMessage &msg) {
  */
 void routeProgram(OSCMessage &msg, int addrOffset) {
     if(msg.fullMatch("/atem/program", 0)) {
-        //Serial.println(msg.getInt(0));
+        int in = msg.getInt(0) - 1;
+        if(in > -1 && in < 16) {
+            setProgramLeds(in);
+            updateLeds();
+        }
     }
 }
 
 void routePreview(OSCMessage &msg, int addrOffset) {
     if(msg.fullMatch("/atem/preview", 0)) {
-        //Serial.println(msg.getInt(0));
+        int in = msg.getInt(0) - 1;
+        if(in > -1 && in < 16) {
+            setPreviewLeds((in));
+            updateLeds();
+        }
     }
 }
 
@@ -127,38 +191,45 @@ void route(OSCBundle &bundle) {
     bundle.route("/atem/preview", routePreview);
 }
 
+
 /**
- * LED Methods
- */
+* LED Methods
+*/
 void setPreviewLeds(int value) {
-    for(int i = 0; i < 16; i++) {
-        previewRow[i] = false;
+    for(bool & i : previewRow) {
+        i = false;
     }
     previewRow[value] = true;
 }
 
 void setProgramLeds(int value) {
-    for(int i = 0; i < 16; i++) {
-        programRow[i] = false;
+    for(bool & i : programRow) {
+        i = false;
     }
     programRow[value] = true;
 }
 
 void updateLeds() {
-    digitalWrite(ledLatch, LOW);
-    shift(getFromBits(programRow, previewRow, 11));
-    shift(getFromBits(macrowRow, auxRow, 11));
-    shift(getFromBits(programRow, previewRow, 7));
-    shift(getFromBits(macrowRow, auxRow, 7));
-    shift(getFromBits(programRow, previewRow, 3));
-    shift(getFromBits(macrowRow, auxRow, 3));
-    shift(getFromBits(programRow, previewRow, 0));
-    shift(getFromBits(macrowRow, auxRow, 0));
-    digitalWrite(ledLatch, HIGH);
+    digitalWrite(driverA[0], LOW);
+    shift(getFromBits(keyRow, groupRow, 3), driverA);
+    shift(getFromBits(functionRow, pipRow, 3), driverA);
+    shift(getFromBits(keyRow, groupRow, 0), driverA);
+    shift(getFromBits(functionRow, pipRow, 0), driverA);
+    digitalWrite(driverA[0], HIGH);
+    digitalWrite(driverB[0], LOW);
+    shift(getFromBits(programRow, previewRow, 11), driverB);
+    shift(getFromBits(macrowRow, auxRow, 11), driverB);
+    shift(getFromBits(programRow, previewRow, 7), driverB);
+    shift(getFromBits(macrowRow, auxRow, 7), driverB);
+    shift(getFromBits(programRow, previewRow, 3), driverB);
+    shift(getFromBits(macrowRow, auxRow, 3), driverB);
+    shift(getFromBits(programRow, previewRow, 0), driverB);
+    shift(getFromBits(macrowRow, auxRow, 0), driverB);
+    digitalWrite(driverB[0], HIGH);
 }
 
-void shift(uint8_t b) {
-    shiftOut(ledData, ledClock, MSBFIRST, b);
+void shift(uint8_t b, const int driver[]) {
+    shiftOut(driver[1], driver[2], MSBFIRST, b);
 }
 
 uint8_t getFromBits(const boolean arrayA[], const boolean arrayB[], int start) {
